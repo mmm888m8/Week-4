fcda6414f02c1b3e6f01258b4a13cf6c57be3616
mmm888m8 <mmm888m8@gmail.com>
2020-08-26 23:35:15 +0800

    Update newProducts.js

--- newProducts.js
+++ newProducts.js
@@ -8,17 +8,30 @@ var app =new Vue({
     tempProduct: {imageUrl: ['']},
     token: '',
     paginationData: {},
-    api: {
+  
+    user: {
+        token: '',
         uuid: 'a1c6ea73-ad09-423e-8d17-9b432e1283a7',
-        path: 'https://course-ec-api.hexschool.io'
-    },
-    
-  } 
-  ,methods:{
+      },
+  } ,
+  created() {
+    // 取得 token 的 cookies
+    // 詳情請見：https://developer.mozilla.org/zh-CN/docs/Web/API/Document/cookie
+    this.user.token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    // 若無法取得 token 則返回 Login 頁面
+    if (this.user.token === '') {
+      window.location = 'Login.html';
+    }
+
+    this.getProducts();
+  },
+  methods:{
     getProducts(page = 1) {
         this.tempProduct = {imageUrl: ['']};
-        const getProductsApiPath = `${this.api.path}/api/${this.api.uuid}/admin/ec/products?page=${page}`
-        axios.get(getProductsApiPath).then(res => {
+        const api = `https://course-ec-api.hexschool.io/api/${this.user.uuid}/admin/ec/products?page=${page}`;
+            //預設帶入 token
+            axios.defaults.headers.common.Authorization = `Bearer ${this.user.token}`;
+        axios.get(api).then(res => {
             this.products = res.data.data;
             this.paginationData = res.data.meta.pagination;
         }).catch(err => {
