9a311fa60c07cf2872959a25c5ffce37a908e196
mmm888m8 <mmm888m8@gmail.com>
2020-08-26 23:13:44 +0800

    add

--- newProducts.js
+++ newProducts.js
@@ -1,34 +1,32 @@
+import pagination from './pagination.js';
+Vue.component('pagination', pagination);
+
 var app =new Vue({
-    el:'#app'
+    el:'#product'
   ,data:{
-    products: [{
-          id: 1586934917210,
-          unit: '台',
-          category: '掌上主機',
-          title: 'Switch',
-          origin_price: 20000,
-          price: 9980,
-          description: '想玩就玩',
-          content: '動森太好玩惹',
-          is_enabled: 1,
-          imageUrl: 'https://images.unsplash.com/photo-1592107761705-30a1bbc641e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
-        },
-        {
-          id: 1196934917910,
-          unit: '台',
-          category: '主機',
-          title: 'PS5 Wifi',
-          origin_price: 29999,
-          description: '次世代超強規格',
-          content: '我也想要換一台 PS5 Wifi',
-          price: 9487,
-          is_enabled: 1,
-          imageUrl: 'https://images.unsplash.com/photo-1592107761705-30a1bbc641e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80',
-        },
-      ],
-    tempProduct:{},
+    productData: [],
+    tempProduct: {imageUrl: ['']},
+    token: '',
+    paginationData: {},
+    api: {
+        uuid: 'a1c6ea73-ad09-423e-8d17-9b432e1283a7',
+        path: 'https://course-ec-api.hexschool.io'
+    },
+    
   } 
   ,methods:{
+    getProducts(page = 1) {
+        this.tempProduct = {imageUrl: ['']};
+        const getProductsApiPath = `${this.api.path}/api/${this.api.uuid}/admin/ec/products?page=${page}`
+        axios.get(getProductsApiPath).then(res => {
+            this.productData = res.data.data;
+            this.paginationData = res.data.meta.pagination;
+        }).catch(err => {
+            console.log(err);
+        });
+    },
+
+
     updateProduct() {
       if (this.tempProduct.id) {
           const id = this.tempProduct.id;
